d307466a96e3efe4675b4f752a0d5eeb9f2b363b
S SUHAS <155213119+SSUHAS4U@users.noreply.github.com>
2024-04-28 01:09:53 +0530

    Add files via upload

---
+++ server/models/employee.js
@@ -0,0 +1,44 @@
+// employee.model.js
+
+const mongoose = require('mongoose');
+
+const EmployeeSchema = mongoose.Schema({
+    ID: {
+        type: String,
+        unique: true,
+        required: true,
+    },
+    name: {
+        type: String,
+        required: true,
+    },
+    department: {
+        type: String,
+        required: true,
+    },
+    mobilenumber: {
+        type: String,
+        required: true,
+    },
+    mail: {
+        type: String,
+        required: true,
+    },
+    leaveStartDate: {
+        type: Date,
+        required: true,
+    },
+    leaveEndDate: {
+        type: Date,
+        required: true,
+    },
+    reasonForLeave: {
+        type: String,
+        required: true, 
+    },
+    status: { type: String, enum: ['Pending', 'Accepted', 'Rejected'], default: 'Pending' }
+});
+
+const EmployeeModel = mongoose.model("Employee Leave", EmployeeSchema);
+
+module.exports = EmployeeModel;
